4742d70df760b09eef7e0620b49c307fc6d99a69
Sergio Galvis <sergioivangalvisesteban@gmail.com>
2023-02-24 22:54:50 -0500

    Create usuarios

--- application/models/UsuariosModel.php
+++ application/models/UsuariosModel.php
@@ -3,7 +3,22 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class UsuariosModel extends CI_Model
 {
+    /**
+     * It takes an array of data and inserts it into the database
+     * 
+     * @param data The data array to be inserted into the database.
+     * 
+     * @return The result of the insert query.
+     */
     public function addUser($data){
         return $this->db->insert('users', $data);
     }
+
+    public function getUser($id_costomer){
+        $this->db->where('id_costomer', $id_costomer);
+        return $this->db->get('users');
+        
+    }
+        
+    
 }
--- js/Usuarios/funciones.js
+++ js/Usuarios/funciones.js
@@ -9,9 +9,6 @@ function insert() {
 		},
         mode: 'no-cors',
 		method: "POST",
-		/* body: JSON.stringify({
-			formulario: formulario,
-		}), */
         body: postData,
 	})
 		.then(function (response) {
@@ -20,6 +17,13 @@ function insert() {
 		})
 		.then(function (json) {
 			// Usamos la información recibida como necesitemos
-			console.log(json);
-		});
+			if(json['resultado'] === 'success') {
+				alert(`Se ha realizado con exito el registro del usuario ${json['data'].first_name} ${json['data'].first_surname}`);
+			}else if (json['resultado'] === 'error') {
+				alert(`El usuario que esta intentando ingresar no puede ser registrado ya quue se encuentra un registro con el mismo número de documento ${json['data'].id_costomer}`);
+			}
+		})
+		.catch(function (error) {
+            console.log(error);
+        });
 }
