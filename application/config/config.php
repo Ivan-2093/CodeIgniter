edeb4b96c0b033e6ad5d5387f008ae0a2dcb6556
Sergio Galvis <73755913+Ivan-2093@users.noreply.github.com>
2023-02-27 08:40:10 -0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ASA.1/';
+/*$config['base_url'] = 'http://localhost/ASA.1/';*/
+$config['base_url'] = 'https://ivan-2093.github.io/CodeIgniter/';
 
 /*
 |--------------------------------------------------------------------------
